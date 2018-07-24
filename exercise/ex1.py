3f0e5d9c1708033e9bb4583d6d4e909d753a8d4b
ChunChiaYang <Vjames861015@gmail.com>
2018-07-24 14:47:20 +0800

    complete ex1.py

--- exercise/ex1.py
+++ exercise/ex1.py
@@ -6,8 +6,17 @@ from lib.stack import Stack
 
 def dec_to_bin(dec):
     # Finish the function
-
+    s=Stack()
+    while dec//2!=0:
+        s.push(dec%2)
+        dec=dec//2
+        if dec==1:
+            s.push(1)
+    binary=[]
+    while not s.isEmpty():
+        binary.append(s.pop())
+        
     return binary
 
-dec_to_bin(42)   # 回傳 101010
-dec_to_bin(100)  # 回傳 1100100
+print(*dec_to_bin(42))   # 回傳 101010
+print(*dec_to_bin(100))  # 回傳 1100100
